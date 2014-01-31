e29aade8b083dceefc5fe627fcc5d4662f973a03
Sergii Bomko <aquila.ua@gmail.com>
2014-01-31 20:51:17 +0200

    changet packages id

--- src/Coding4Fun.TfsAnalytics/Guids.cs
+++ src/Coding4Fun.TfsAnalytics/Guids.cs
@@ -4,7 +4,7 @@ namespace Coding4Fun.TfsAnalyticsPackage
 {
 	static class GuidList
 	{
-		public const string guidCoding4Fun_TfsAnalyticsPkgString = "2811c71f-e136-4da6-befb-17614aad1297";
+		public const string guidCoding4Fun_TfsAnalyticsPkgString = "2811c71f-e136-4da6-befb-17614aad1296";
 		public const string guidCoding4Fun_TfsAnalyticsCmdSetString = "9a4b587d-8ba4-46bf-8ea6-6b204e7d54d9";
 
 		public static readonly Guid guidCoding4Fun_TfsAnalyticsCmdSet = new Guid(guidCoding4Fun_TfsAnalyticsCmdSetString);
